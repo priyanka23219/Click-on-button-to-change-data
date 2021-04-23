700f68cb1b51ca5cc89893c7ce42ded102c29d17
Priyanka <priyanka.23219@gmail.com>
2021-04-23 10:40:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'btnClick';
+  newPost = ' CONTENT';
+  newPost1 = 'CONTENT1';
+  newPost2 = ' CONTENT2';
+  newPost3 = ' CONTENT3';
+  newPost4 = ' CONTENT4';
+  newPost5 = ' CONTENT5';
+  newPost6 = ' CONTENT6';
+  newPost7 = ' CONTENT7';
+  newPost8 = ' CONTENT8'
+  newPost9 = ' CONTENT9';
+  newPost10 = ' CONTENT10';
+  newPost11 = ' CONTENT11';
+  newPost12 = ' CONTENT12';
+
+onAddPost(){
+this.newPost = 'Hello from Angular';
+}
+onAddPost1(){
+  this.newPost1 = 'Hello from Angular';
+  }
+  onAddPost2(){
+    this.newPost2 = 'Hello from Angular';
+    }
+    onAddPost3(){
+      this.newPost3 = 'Hello from Angular';
+      }
+      onAddPost4(){
+        this.newPost4 = 'Hello from Angular';
+        }
+        onAddPost5(){
+          this.newPost5 = 'Hello from Angular';
+          }
+          onAddPost6(){
+            this.newPost6 = 'Hello from Angular';
+            }
+            onAddPost7(){
+              this.newPost7 = 'Hello from Angular';
+              }
+            onAddPost8(){
+              this.newPost8 = 'Hello from Angular';
+              }
+              onAddPost9(){
+                this.newPost9 = 'Hello from Angular';
+                }
+                onAddPost10(){
+                  this.newPost10 = 'Hello from Angular';
+                  }
+                  onAddPost11(){
+                    this.newPost11 = 'Hello from Angular';
+                    }
+                    onAddPost12(){
+                      this.newPost12 = 'Hello from Angular';
+                      }
+
 }
